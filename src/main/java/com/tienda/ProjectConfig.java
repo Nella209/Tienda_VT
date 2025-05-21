360d4c58e0c49c117f5b6aeb99dda7656e47321d
Nella <147450405+Nella209@users.noreply.github.com>
2025-05-21 02:49:19 -0600

    Semana 1: Introducción

---
+++ src/main/java/com/tienda/ProjectConfig.java
@@ -0,0 +1,25 @@
+package com.tienda;
+
+
+@configuration 
+public class ProjectConfig implements WebMvcConfigurer {
+    
+    /* Los siguiente métodos son para implementar el tema de seguridad dentro del proyecto */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/ejemplo2").setViewName("ejemplo2");
+    }
+    /* El siguiente método se utilizar para publicar en la nube, independientemente  */
+    @Bean
+    public SpringResourceTemplateResolver templateResolver_0() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setPrefix("classpath:/templates");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode(TemplateMode.HTML);
+        resolver.setOrder(0);
+        resolver.setCheckExistence(true);
+        return resolver;
+    }
+    
+}
