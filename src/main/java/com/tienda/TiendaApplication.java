27307f3091c41559721139330594785a56ba1a02
Nella <147450405+Nella209@users.noreply.github.com>
2025-06-05 23:13:40 -0600

    Semana 1: Introducción Corrección

--- src/main/java/com/tienda/TiendaApplication.java
+++ src/main/java/com/tienda/TiendaApplication.java
@@ -2,6 +2,9 @@ package com.tienda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
 
 @SpringBootApplication
 public class TiendaApplication {
