85fc86a5024f7b42141d5568a62fca30f2e76b0f
Nella <Nella@DESKTOP-C90VLIP>
2025-07-19 01:20:54 -0600

    Semana 4: Listado y fragmentos

---
+++ src/main/java/com/tienda/service/CategoriaService.java
@@ -0,0 +1,30 @@
+package com.tienda.service;
+
+import com.tienda.domain.Categoria;
+import com.tienda.repository.CategoriaRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CategoriaService {
+
+    // Permite crear una unica instancia d categoriaRepository, y la crea de manera automatica sin hacerle un new
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @Transactional(readOnly = true)
+    public List<Categoria> getCategorias(boolean activo) {
+
+        var lista = categoriaRepository.findAll();
+        /*Solo porque existe el activo
+        Se valida solo si se quieren las categorias activas*/
+        if (activo) {
+            lista.removeIf(c -> !c.isActivo());
+
+        }
+        return lista;
+
+    }
+}
